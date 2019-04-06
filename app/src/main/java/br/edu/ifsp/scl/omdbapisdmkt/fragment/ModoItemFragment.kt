e3916cdc879c34396657a9a254bb54be0c28b65d
Cornachione <leonardo.cornachione@dxc.com>
2019-04-06 12:29:07 -0300

    Ajuste Scrollview

--- app/src/main/java/br/edu/ifsp/scl/omdbapisdmkt/fragment/ModoItemFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/omdbapisdmkt/fragment/ModoItemFragment.kt
@@ -6,8 +6,6 @@ import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import br.edu.ifsp.scl.omdbapisdmkt.R
 import br.edu.ifsp.scl.omdbapisdmkt.data.OMDb
 import br.edu.ifsp.scl.omdbapisdmkt.fragment.ModoApp.codigosMensagen.RESPOSTA_BUSCA
